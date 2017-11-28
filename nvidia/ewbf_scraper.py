a6c2742f5af77d1b96b3f0fa640465ac2c69fd57
Greg Perry <gsperry2011@gmail.com>
2017-11-28 18:40:39 -0500

    writes output to file seperately GPU by newlines. overwrites file each run

--- nvidia/ewbf_scraper.py
+++ nvidia/ewbf_scraper.py
@@ -1,8 +1,16 @@
 #import the library used to query a website
 import urllib2
 
-#specify the url
+#########################
+#specify the url to scrap
 url = "http://10.250.1.100:42000"
+
+# file to write output for prometheus to read
+outputfile = '~/testoutput.txt'
+
+
+
+#########################
 
 page = urllib2.urlopen(url)
 
@@ -20,37 +28,35 @@ gpu_statistic_array = []
 # Tracking current GPU ID
 current_gpu = 0
 
-for table_row in statistics_table:
+# open our file for writing. It will overwrite the next time we open it.
+with open('%outputfile', 'w') as output:
 
-    # what we will search the table_row for
-    gpu_id = 'gpu' + str(current_gpu)
+    for table_row in statistics_table:
 
-    # converting from bs4.tag to string to be checked for gpu_id
-    current_row = str(table_row)
+        # what we will search the table_row for
+        gpu_id = 'gpu' + str(current_gpu)
 
-    # find rows containing GPUs
-    if gpu_id in current_row:
-
-#        print 'the table_row is:'
-#        print table_row
-        print '\n'
-
-        # must target table_row and not current_row as find_all requires the bs4 object.
-        gpu_stats = table_row.find_all('td')
-
-        # setting variables for sanity. this removes the final <td> tag and contains the actual data only.
-        gpu_name = gpu_stats[0].text
-        gpu_temp = gpu_stats[1].text
-        gpu_power = gpu_stats[2].text
-        gpu_speed = gpu_stats[3].text
-        gpu_efficiency = gpu_stats[4].text
-        gpu_acceptedshares = gpu_stats[5].text
-        gpu_rejectedshares = gpu_stats[6].text
-
-        print gpu_name, gpu_temp, gpu_power, gpu_speed, gpu_efficiency, gpu_acceptedshares, gpu_rejectedshares
+        # converting from bs4.tag to string to be checked for gpu_id
+        current_row = str(table_row)
         
-        # increment to go to next GPU
-        current_gpu = current_gpu + 1
-    
+        # find rows containing GPUs
+        if gpu_id in current_row:
 
-print gpu_statistic_array
+        
+            # must target table_row and not current_row as find_all requires the bs4 object.
+            gpu_stats = table_row.find_all('td')
+            
+            # writing output to file. lines will not be trampled as we keep the file open for the duration of the script run.
+            output.write( "gpu_name: %s, gpu_temp: %s, gpu_power: %s, gpu_speed: %s, gpu_efficiency: %s, gpu_acceptedshares: %s, gpu_rejectedshares: %s\n" % \
+                       (gpu_stats[0].text,
+                        gpu_stats[1].text,
+                        gpu_stats[2].text,
+                        gpu_stats[3].text,
+                        gpu_stats[4].text,
+                        gpu_stats[5].text,
+                        gpu_stats[6].text))
+
+        
+            # increment to go to next GPU
+            current_gpu = current_gpu + 1
+        
